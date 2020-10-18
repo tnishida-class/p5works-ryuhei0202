57b4eaee2c93f2dfa6182c208a10b80f34abd1ee
Ryuhei Matsumoto <ryuheim2268@gmail.com>
2020-10-19 01:38:11 +0900

    Update 3 files

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -7,8 +7,6 @@ function draw() {
   strokeWeight(4);
   fill(178, 100, 162);
   triangle(12,120,116,120,116,10);
-
-  
   fill(94, 185, 84);
   triangle(0,108,106,108,106,0);
   fill(255);
--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -4,3 +4,17 @@ function setup(){
     // BLANK[1]
   }
 }
+
+/* 予習での解答
+function setup() {
+  createCanvas(220, 220);
+  for(let i = 0; i < 10; i++){
+    noFill();
+    ellipse(110, 110, (i + 1) * 10);
+    if(i < 5){
+      stroke(0, 0, 255);
+    } else {
+      stroke(255, 0, 0);
+    }
+  }
+} */

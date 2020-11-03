9b84f82a8db17eb7670885715ae36443c8feec7e
Ryuhei Matsumoto <ryuheim2268@gmail.com>
2020-11-03 18:28:38 +0900

    Update sketch.js

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,15 +1,18 @@
-// ギリシャ国旗
 function setup() {
   const blue = color(0, 51, 160);
   createCanvas(270, 180);
-  // noStroke();
+  noStroke();
   background(255);
 
-  let d = height / 9; // 縞1本の太さ
+  let d = height / 9;
 
   for(let i = 0; i < 9; i++){
-    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, (i + 1) * d);
+    if(i % 2 == 0) {
+      fill(blue);
+    } else {
+      fill(255);
+    }
+    rect(0, i * d, width, d);
   }
 
   fill(blue);
@@ -17,5 +20,6 @@ function setup() {
   rect(0, 0, size, size);
 
   fill(255);
-  // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  rect(0, d * 2, d * 5, d);
+  rect(d * 2, 0, d, d * 5);
 }

7ad87dd2b5ddeca150118def77c19b246e7dd4bb
Ryuhei Matsumoto <ryuheim2268@gmail.com>
2020-11-24 09:51:28 +0900

    Update finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -2,12 +2,27 @@
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw(){
-  background(160, 192, 255);
+  background(120);
+  animation();
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+function animation() {
+  const stride = 30;
+
+  for (let x = 0; x <= width; x += stride) {
+    for (let y = 0; y <= height; y += stride) {
+      const d = dist(x, y, mouseX, mouseY);
+      const size = d / 8;
+      if(mouseX <= width / 2) {
+        fill(255);
+      } else {
+        fill(0);
+      }
+      ellipse(x, y, size);
+    }
+  }
 }
